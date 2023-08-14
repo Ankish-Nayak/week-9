df67969ed934d70d3ba79d0f54cf387d60d6cf92
Ankish Nayak <ankish2000nayak@gmail.com>
2023-08-14 15:17:11 +0530

    hello2

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use("/admin", adminRouter)
 app.use("/user", userRouter)
 app.get('/', (req, res) => {
-	res.json({msg: "say again hello world"});
+	res.json({msg: "say again hello world2"});
 });
 
 // Connect to MongoDB
